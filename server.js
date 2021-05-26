1538298adb439da94cbd2778f0ca212c2dcfe2c9
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2021-05-26 23:56:45 +0300

    fix: some bugs with deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require('express');
  app.use(express.static(path.join(__dirname, 'build')));
  
  
- app.get('/*', function (req, res) {
+ app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
  app.listen(port);
